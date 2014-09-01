4667b2fcd0da74e4012bc9ec10548af48e22bfa6
bakasajoshua <baksajoshua09@gmail.com>
2014-09-01 17:30:26 +0300

    first mail

--- application/modules/mail/controllers/mail.php
+++ application/modules/mail/controllers/mail.php
@@ -1,16 +1,27 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Email extends MX_Controller
+class Mail extends MX_Controller
 {
 
 	function __construct()
 	{
 		parent::__construct();
+		
+		$this->load->model('mail_model');
 	}
 
 	function index()
 	{
-		$this->load->view('default');
+		$this->load->view('email_view');
+	}
+
+	public function email_details()
+	{
+		$recepient 	= $this->input->post('recipient');
+		$subject 	= $this->input->post('subject');
+		$message 	= $this->input->post('message');
+
+		$this->send_email($recepient, $subject, $message);
 	}
 
 	public function send_email($recepient, $subject, $message)
@@ -21,14 +32,14 @@ class Email extends MX_Controller
 			'protocol' => 'smtp',
 			'smtp_host' => 'ssl://smtp.googlemail.com',
 			'smtp_port' => 465,
-			'smtp_user' => "chrisrichards@gmail.com",
-			'smtp_pass' => "bakasaSUN"
+			'smtp_user' => "chrisrichrads@gmail.com",
+			'smtp_pass' => "joshuaSUN"
 			);
 		
 		$this->load->library('email', $config);
 		$this->email->set_newline("\r\n");
 
-		$this->email->from('lims.eidvl@gmail.com', 'EID LIMS');
+		$this->email->from('chrisrichrads@gmail.com', 'SU Notification');
 		$this->email->to($recepient);
 		$this->email->subject($subject);
 		$this->email->message($message);
@@ -37,7 +48,7 @@ class Email extends MX_Controller
 		
 		if($this->email->send())
 			{	
-
+				$this->mail_model->store_sent_email($recepient, $subject, $message, $time);
 				
 			} else 
 			{
--- application/modules/mail/models/mail_model.php
+++ application/modules/mail/models/mail_model.php
@@ -4,12 +4,21 @@ class Mail_model extends CI_Model {
 
     function __construct()
     {
-        // Call the Model constructor
+        
         parent::__construct();
     }
 
-    function defaultfunction($param1, $param2)
+    function store_sent_email($recepient, $subject, $message, $tim)
     {
-        
+       $emails = array(
+			'id' 		   =>   NULL,
+			'recipients'   =>   $recepient,
+			'subject'      =>   $subject,
+			'message'      =>   $message,
+			'date'   	   =>   $tim,
+			'sent_status'  =>   1
+			);
+
+		$insert = $this->db->insert('mailerlog', $emails); 
     }
 }
