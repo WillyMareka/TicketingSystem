1699e736a22a3b90a0f7fb1ce893e06ff728af55
johnotaalo <john.otaalo@strathmore.edu>
2014-09-24 12:31:48 +0300

    Just some changes

--- dbv/data/schema/students.sql
+++ dbv/data/schema/students.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `students` (
-  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `othernames` varchar(255) NOT NULL,
@@ -8,6 +8,7 @@ CREATE TABLE `students` (
   `student_email` varchar(100) NOT NULL,
   `parent_email` varchar(100) NOT NULL,
   `location` varchar(250) NOT NULL,
+  `photo` varchar(255) NOT NULL,
   `admission_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`student_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
--- dbv/data/schema/user.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `user` (
-  `name` varchar(255) NOT NULL,
-  `age` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
